e5521dd092cd7159e4a8025a320f6bb697fc952e
manhnv <manhnv@debian.168.230.10.non-exists.ptr.local>
2025-08-26 16:23:45 +0700

    Update SP

--- src/llamafactory/train/tuner.py
+++ src/llamafactory/train/tuner.py
@@ -11,7 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+try:
+    import unsloth
+except:
+    pass
 import os
 import shutil
 from typing import TYPE_CHECKING, Any, Optional
