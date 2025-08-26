86ddee31af018ba13227b0899eeef2570afa7241
manhnv <manhnv@debian.168.230.10.non-exists.ptr.local>
2025-08-26 13:35:24 +0700

    Update SP

--- src/llamafactory/data/data_utils.py
+++ src/llamafactory/data/data_utils.py
@@ -157,6 +157,21 @@ def setup_fs(path: str, anon: bool = False) -> "fsspec.AbstractFileSystem":
 
     return fs
 
+# modified from https://github.com/jzhang38/EasyContext/
+def preprocess_sp_dataset(seq_ids, world_size, sequence_parallel_mode):
+    if sequence_parallel_mode == "zigzag-ring":
+        step = len(seq_ids) // (2 * world_size)
+        value_chunks = [seq_ids[s : s + step] for s in range(0, len(seq_ids), step)]
+        local_values = list()
+        for rank in range(world_size):
+            local_values.append(value_chunks[rank] + value_chunks[2 * world_size - rank - 1])
+        return local_values
+    elif sequence_parallel_mode == "ulysses":
+        step = len(seq_ids) // world_size
+        local_values = [seq_ids[s : s + step] for s in range(0, len(seq_ids), step)]
+        return local_values
+    else:
+        raise NotImplementedError("Other sequence parallel modes are to be implemented.")
 
 def _read_json_with_fs(fs: "fsspec.AbstractFileSystem", path: str) -> list[Any]:
     r"""Helper function to read JSON/JSONL files using fsspec."""
