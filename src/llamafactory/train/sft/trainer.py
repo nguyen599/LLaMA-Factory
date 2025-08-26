26af744120119adf33df047533cd42714dbcd7b5
manhnv <manhnv@debian.168.230.10.non-exists.ptr.local>
2025-08-26 15:11:27 +0700

    Update SP

--- src/llamafactory/train/sft/trainer.py
+++ src/llamafactory/train/sft/trainer.py
@@ -118,11 +118,12 @@ class CustomSeq2SeqTrainer(Seq2SeqTrainer):
     def compute_loss(self, model, inputs, return_outputs=False, *args, **kwargs):
         print('CHECK in compute_loss', self.model.sequence_parallel_group)
         if self.model.sequence_parallel_group is None:  # no sequence parallel, compute as it is
-            print(inputs)
+            # print(inputs)
             return super().compute_loss(model, inputs, **kwargs)
         else:
-            print('input', inputs)
-            print(dir(inputs))
+            print('atten_mask', inputs['attention_mask'].shape)
+            print('input_ids', inputs['input_ids'].shape)
+            # print(dir(inputs))
             # compute loss without shift labels, as we have already shifted labels in data processing when using sequence parallel
             _, outputs = super().compute_loss(model, inputs, return_outputs=True, **kwargs)
             # Flatten the tokens
